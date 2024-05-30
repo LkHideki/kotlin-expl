5711203f5f1c3f38c47597ba23b9c854992ad4bf
LkHideki <lucas.hideki.07@gmail.com>
2024-05-30 10:17:25 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pequenas doses de Kotlin
+
+Repositório diário que não será commitado todo dia.
+
+Vou registrando o meu avanço em kotlin.
---
+++ a0-basicos/a01.kt
@@ -0,0 +1,13 @@
+/**
+ * Para rodar o arquivo kotlin:
+ * 
+ * 1. Compile o arquivo para .jar (Java ARchive):
+ * $ kotlinc <arquivo.kt> -include-runtime -d <arquivo.jar>
+ * 
+ * 2. Rode o arquivo .jar:
+ * $ java -jar <arquivo.jar>
+ */
+
+fun main() {
+  println("Olá, mundo!")
+}
