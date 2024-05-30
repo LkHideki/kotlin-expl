11ba4edcf76113158e534c6dc0586812277a768f
LkHideki <lucas.hideki.07@gmail.com>
2024-05-30 10:35:52 -0300

    algumas convenções

---
+++ a0-basicos/a02-design.kt
@@ -0,0 +1,14 @@
+/**
+ * 4 espaços de indentação
+ */
+fun main(args: Array<String>) {
+	println("Indentação com tab de 4 espaços")
+
+    // blabla
+    println("Espaço após o // do comentário")
+
+    // { na mesma linha da assinatura.
+    fun demo() {
+        println("oioi")
+    }
+}
